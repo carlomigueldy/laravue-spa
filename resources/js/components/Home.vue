61ed7c7d3ba0f2c1701d6858df266b2a1a4cf3dc
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-09-11 08:44:37 +0800

    boilerplate

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-lg-12 px-5">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            This is the home view.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
---
+++ resources/js/components/MainApp.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="main">
+        <Header />
+        <div class="container my-4">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from './Header'
+
+export default {
+    name: 'MainApp',
+    components: { Header }
+}
+</script>
